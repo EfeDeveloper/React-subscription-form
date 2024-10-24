984a5ffd48dd98991de1c50c3425f42442b49c0e
EfeDeveloper <edwinvilla_adsi@outlook.com>
2024-10-24 12:22:17 -0500

    feat ✨: Configure Tailwind CSS and PostCSS for project
    - Added configuration for postcss.
    - The main goal of the changes is to configure Tailwind CSS for a project.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Ajusta para TypeScript y JSX/TSX
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
