d41873c36ac6f8b6cc228376cf109f1c5a4e26f7
EfeDeveloper <edwinvilla_adsi@outlook.com>
2024-11-15 10:36:59 -0500

    style 💄: Refactored margin styles for consistency.
    - Refactored margin styles for consistency.

--- src/commons/components/Layout.tsx
+++ src/commons/components/Layout.tsx
@@ -1,17 +1,16 @@
 import { AppSidebar } from "@/commons/components/AppSidebar";
 import Footer from "@/commons/components/Footer";
 import Header from "@/commons/components/Header";
-import { SidebarProvider, SidebarTrigger } from "@/core/components/ui/sidebar";
+import { SidebarProvider } from "@/core/components/ui/sidebar";
 
 const FormLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <SidebarProvider>
       <AppSidebar />
       <main className="w-dvw">
-        <SidebarTrigger />
         <section className="grid grid-rows-[auto_1fr_auto] min-h-[100dvh]">
           <Header />
-          <section className="m-2 md:m-10">{children}</section>
+          <section className="mx-2 md:mx-10">{children}</section>
           <Footer />
         </section>
       </main>
